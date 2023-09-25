8d0bcc1ed3b5ec336adbc34b7dd754decd940838
ajay450d <ajay450d@gmail.com>
2023-09-25 09:01:21 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WeatherComponent from './Components/WeatherComponent/WeatherComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <React.Fragment>
+    <WeatherComponent/>
+   </React.Fragment>
+  )
+}
+
+export default App
